5ad46cf60cd9e4b9fef3b024c3ce3c1c38e2861a
Andreas <Andreas@AS_Hjemme_PC>
2015-02-16 20:54:10 +0100

    påbegyndt exception

--- 12_CDIO_del1/src/data/IOperatorDAO.java
+++ 12_CDIO_del1/src/data/IOperatorDAO.java
@@ -1,13 +1,13 @@
 package data;
 
 	public interface IOperatorDAO {
-//
-//OperatoerDTO getOperatoer(int oprId) throws DALException;
+
+//String OperatorDTO getOperatoer(int oprId) throws DALException;
 //
 //List<OperatoerDTO> getOperatoerList() throws DALException;
 //
 //void createOperatoer(OperatoerDTO opr) throws DALException;
 //
 //void updateOperatoer(OperatoerDTO opr) throws DALException;
-//
+
 }
