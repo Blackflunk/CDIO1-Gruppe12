98ffa103072c7cfa61b6aa40fb2a36e8cc958af4
Dtu Andreas <s082853@student.dtu.dk>
2015-02-16 16:28:55 +0100

    OpertorDTO lavet

--- 12_CDIO_del1/src/data/OperatorDTO.java
+++ 12_CDIO_del1/src/data/OperatorDTO.java
@@ -2,4 +2,49 @@ package data;
 
 public class OperatorDTO {
 
+	private int oprId;
+	private String oprNavn;
+	private String ini;
+	private String cpr;
+	private boolean admin;
+	
+	public OperatorDTO(int oprId, String oprNavn, String ini, String cpr, boolean admin){
+		this.admin = admin;
+		this.cpr = cpr;
+		this.ini = ini;
+		this.oprNavn = oprNavn;
+		this.oprId = oprId;
+	}
+	
+	public int getOprId() {
+		return oprId;
+	}
+	public void setOprId(int oprId) {
+		this.oprId = oprId;
+	}
+	public String getOprNavn() {
+		return oprNavn;
+	}
+	public void setOprNavn(String oprNavn) {
+		this.oprNavn = oprNavn;
+	}
+	public String getIni() {
+		return ini;
+	}
+	public void setIni(String ini) {
+		this.ini = ini;
+	}
+	public String getCpr() {
+		return cpr;
+	}
+	public void setCpr(String cpr) {
+		this.cpr = cpr;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	String password;
 }
