8f255d123241e8d1b5e2955d83a5e2a2ca05fc3a
AoschkA <jonaspraem@gmail.com>
2015-02-17 11:02:33 +0100

    IUI for real

--- 12_CDIO_del1/src/boundary/IUI.java
+++ 12_CDIO_del1/src/boundary/IUI.java
@@ -1,5 +1,46 @@
 package boundary;
 
-public class IUI {
+public interface IUI {
+	// Inden logget ind.
+	void endMessage();
 
+	//Oprettelse af bruger
+	String getUserName();
+	String cprNumber();
+	void showPassword(String password);
+	
+	//Fejlbeskeder
+	void changeNameFailed();
+	void changeNameSucces();
+	void changePasswordFailed();
+	void changePasswordSucces();
+	void loginSuccesMessage();
+	void loginFailedMessage();
+	void logoutSuccesMessage();
+	void logoutFailedMessage();
+	void createUserFailedMessage();
+	void createUserSuccesMessage();
+	
+	//Vægtprogram
+	int insertWeight();
+	int insertTara();
+	void showResult();
+	void showWeightMessage();
+	
+	//Login valgmuligheder
+	String changeName();
+	String changePassword();
+
+	//Adminstrations menu
+	void showUserList(int[] IDList,String[] nameList);
+	int deleteUser();
+	int makeUserAdmin();
+
+	//Menuer
+	int showUserMenu();
+	int showManageMenu();
+	int showAdminMenu();
+	int showMainMenu();
+	
+	
 }
